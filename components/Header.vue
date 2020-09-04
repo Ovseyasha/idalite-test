1a01b4df123e863253ef5fe187135979d4ef7142
ovseyasha <ovseyasha@mail.ru>
2020-09-04 13:36:03 +0300

    project init

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>TestList</h1>
+    <Cart />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Category />
+    <ProductList />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
